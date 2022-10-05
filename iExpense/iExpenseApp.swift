4f64e1ddce20cb34c3513f4cc1e24f5ecbed73b3
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-05 15:24:46 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Zaid Neurothrone on 2022-10-05.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
