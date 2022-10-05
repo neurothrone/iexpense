4acfac3bc084ba14e9d2a519c50d0da19bd6177e
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-05 15:26:06 +0200

    rearrange project structure

--- iExpense/App/iExpenseApp.swift
+++ iExpense/App/iExpenseApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct iExpenseApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
